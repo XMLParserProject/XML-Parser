edea9e72dccfc15b551e81aee8c2d0fbc7b385c6
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2023-12-13 21:50:43 +0200

    Create Helpers.cpp

---
+++ Level_1/Helpers.cpp
@@ -0,0 +1,41 @@
+#include <string>
+using namespace std;
+
+class Helpers
+{
+private:
+    
+public:
+    string removeUnwantedSpaces(string XMLText);
+};
+
+
+string  Helpers::removeUnwantedSpaces(string XMLText){
+    string output="";
+    int startOfXML=XMLText.find('<');
+    output+=XMLText[startOfXML];
+    char previousCharacter=output[0];
+
+    for(int i=startOfXML+1;i<XMLText.length();i++){
+
+        if(XMLText[i] == ' ' || XMLText[i] == '\n'){
+            if(previousCharacter == ' ' || previousCharacter == '<' || previousCharacter == '>'){
+                continue;
+            }
+            output+=' ';
+        }else if(XMLText[i] == '<' || XMLText[i] == '>'){
+            if(previousCharacter == ' '){
+                output=output.substr(0,output.length()-1);
+                output+=XMLText[i];
+            }else{
+                output+=XMLText[i];
+            }
+
+        }else{
+            output+=XMLText[i];
+        }
+        previousCharacter = output[output.length()-1];
+    }
+
+    return output;  
+} 
