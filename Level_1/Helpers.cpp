9265f319ceba6b2e032c8f7bad28da00fdc800e5
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2023-12-13 23:49:01 +0200

    Update Helpers.cpp

--- Level_1/Helpers.cpp
+++ Level_1/Helpers.cpp
@@ -1,33 +1,31 @@
-#include <string>
 #include "Helpers.h"
-using namespace std;
 
-string  Helpers::removeUnwantedSpaces(string XMLText){
-    string output="";
-    int startOfXML=XMLText.find('<');
-    output+=XMLText[startOfXML];
-    char previousCharacter=output[0];
+std::string Helpers::removeUnwantedSpaces(std::string XMLText) {
+    std::string output = "";
+    int startOfXML = XMLText.find('<');
+    output += XMLText[startOfXML];
+    char previousCharacter = output[0];
 
-    for(int i=startOfXML+1;i<XMLText.length();i++){
+    for (int i = startOfXML + 1; i < XMLText.length(); i++) {
 
-        if(XMLText[i] == ' ' || XMLText[i] == '\n'){
-            if(previousCharacter == ' ' || previousCharacter == '<' || previousCharacter == '>'){
+        if (XMLText[i] == ' ' || XMLText[i] == '\n') {
+            if (previousCharacter == ' ' || previousCharacter == '<' || previousCharacter == '>') {
                 continue;
             }
-            output+=' ';
-        }else if(XMLText[i] == '<' || XMLText[i] == '>'){
-            if(previousCharacter == ' '){
-                output=output.substr(0,output.length()-1);
-                output+=XMLText[i];
-            }else{
-                output+=XMLText[i];
+            output += ' ';
+        } else if (XMLText[i] == '<' || XMLText[i] == '>') {
+            if (previousCharacter == ' ') {
+                output = output.substr(0, output.length() - 1);
+                output += XMLText[i];
+            } else {
+                output += XMLText[i];
             }
 
-        }else{
-            output+=XMLText[i];
+        } else {
+            output += XMLText[i];
         }
-        previousCharacter = output[output.length()-1];
+        previousCharacter = output[output.length() - 1];
     }
 
-    return output;  
-} 
+    return output;
+}
