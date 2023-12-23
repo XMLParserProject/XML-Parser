48c1078ec5a66287a79bffd7482ef2933ad181c0
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2023-12-23 14:09:46 +0200

    Update Helpers.cpp

--- Level_1/Helpers.cpp
+++ Level_1/Helpers.cpp
@@ -1,7 +1,10 @@
 #include "Helpers.h"
 
-std::string Helpers::removeUnwantedSpaces(std::string XMLText) {
-    std::string output = "";
+
+// if you use this method in any incorrect xml text it will output error
+
+string Helpers::removeUnwantedSpaces(string XMLText) {
+    string output = "";
     int startOfXML = XMLText.find('<');
     output += XMLText[startOfXML];
     char previousCharacter = output[0];
@@ -13,7 +16,7 @@ std::string Helpers::removeUnwantedSpaces(std::string XMLText) {
                 continue;
             }
             output += ' ';
-        } else if (XMLText[i] == '<' || XMLText[i] == '>') {
+        }else if(XMLText[i] == '<' || XMLText[i] == '>' || (XMLText[i] == '?' && i<XMLText.length()-1 && XMLText[i+1]== '>')  || (XMLText[i] == '?' && i>0 && XMLText[i-1]== '<')) {
             if (previousCharacter == ' ') {
                 output = output.substr(0, output.length() - 1);
                 output += XMLText[i];
