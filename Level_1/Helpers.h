7484172968603088cd421525492f3cf510ae126f
AhmedHossamX <130851475+AhmedHossamX@users.noreply.github.com>
2023-12-29 14:24:40 +0200

    Update Helpers.h

--- Level_1/Helpers.h
+++ Level_1/Helpers.h
@@ -1,16 +1,17 @@
-
 // Tree.h
 #pragma once
-
 #include <string>
 #include <fstream>
+#include "vector"
 
 using namespace std;
 
 class Helpers {
 private:
+    string convertXML(const string& input);
 
 public:
     string removeUnwantedSpaces(string XMLText);
-    void saveFile(string text,string path);
+    string prepare(string XMLText);
+    void saveFile(string text,string path);
 };
