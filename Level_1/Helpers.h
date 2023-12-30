09b2e738f1ad9da73b025fd4a4deb64abc68b3b3
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2023-12-30 11:41:17 +0200

    Update Helpers.h

--- Level_1/Helpers.h
+++ Level_1/Helpers.h
@@ -1,8 +1,8 @@
-// Tree.h
 #pragma once
 #include <string>
 #include <fstream>
-#include "vector"
+#include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -11,7 +11,14 @@ private:
     string convertXML(const string& input);
 
 public:
+    // Convert XML to a Vector of elements
+    vector<string> convertXMLToVector(string XMLText);
+    // if you use this method in any incorrect xml text it will output error
     string removeUnwantedSpaces(string XMLText);
+
     string prepare(string XMLText);
+
+    string addQuotes(string element);
+
     void saveFile(string text,string path);
 };
