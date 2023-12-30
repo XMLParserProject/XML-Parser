fad36fc6ba58d3eaa6e4b6f54655ab01016d7d83
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2023-12-30 11:37:21 +0200

    Update XMLToJson.cpp

--- Level_1/XMLToJson.cpp
+++ Level_1/XMLToJson.cpp
@@ -4,8 +4,7 @@
 using namespace std;
 
 XMLToJson::XMLToJson(string xmlText){
-    Helpers helper;
-    setXMLText(helper.removeUnwantedSpaces(xmlText));
+    setXMLText(helper.prepare(xmlText));
 }
 
 
@@ -28,7 +27,7 @@ string XMLToJson::getJSONText(){
 string XMLToJson::convertToJson(){
     string output = "{";
     int level=0;
-    vector<string> elements = ToVector(convertToTree().preOrder());
+    vector<string> elements = helper.convertXMLToVector(this->XMLText);
     stack<char> openedBrackets;
     int mark=0;
 
@@ -37,7 +36,7 @@ string XMLToJson::convertToJson(){
             continue;
         if((elements[i].length()>3) && (elements[i].substr(0,4) == ";<?>")){
             level--;
-            output+= addQuotes(elements[i].substr(4,elements[i].length()-4)) ;
+            output+= helper.addQuotes(elements[i].substr(4,elements[i].length()-4)) ;
             output[mark]= '\0';
             if(i < (elements.size()-1) && elements[i+1].length() > 0 && (elements[i+1][0] != '/')){
                 output+=',';
@@ -57,7 +56,7 @@ string XMLToJson::convertToJson(){
                 level--;
             }
             level++;
-            output+= "\n" + HandleIndentation(level) + addQuotes(elements[i]) + ": {";
+            output+= "\n" + HandleIndentation(level) + helper.addQuotes(elements[i]) + ": {";
             mark=output.length()-1;
             openedBrackets.push('{');
         }
@@ -66,86 +65,6 @@ string XMLToJson::convertToJson(){
 
     return output;
 }
-
-Tree XMLToJson::convertToTree(){
-    Tree xmlTree;
-    int firstPointer=0;
-    int secondPointer=0;
-    string lastvalue="";
-    char currentCharacter;
-    bool betweenBrackets=false;
-    stack<string> openedtages;
-
-        while(secondPointer < XMLText.length()){
-        currentCharacter = XMLText[secondPointer];
-        if(currentCharacter == '<'){
-            secondPointer++;
-            firstPointer=secondPointer;
-            betweenBrackets=true;
-        }else if(currentCharacter == '>'){
-            if(XMLText[firstPointer] == '/'){
-                string closedtag = XMLText.substr(firstPointer+1,secondPointer - firstPointer - 1);
-                if(closedtag == openedtages.top()){
-                    if(lastvalue.length()>0){
-                        xmlTree.insert(";<?>"+lastvalue,openedtages.top());
-                        lastvalue="";
-                    }
-                    else
-                        xmlTree.insert("/"+closedtag,openedtages.top());
-                }
-                openedtages.pop();
-            }else{
-                string openedtag=XMLText.substr(firstPointer,secondPointer - firstPointer);
-                if(XMLText[secondPointer-1] != '/'){
-                if(openedtag.find(' '))
-                    openedtag = openedtag.substr(0,openedtag.find(' '));
-
-                if(openedtag != "xml" && openedtag != "?xml"){                      
-                if(openedtages.empty())
-                    xmlTree.insert(openedtag,openedtag);
-                else
-                    xmlTree.insert(openedtag,openedtages.top());
-                openedtages.push(openedtag);
-                } 
-                }
-            }
-            betweenBrackets=false;
-            firstPointer=secondPointer+1;
-        }else{
-            if(!betweenBrackets && XMLText[secondPointer+1] == '<')
-                lastvalue = XMLText.substr(firstPointer,secondPointer - firstPointer + 1);
-        }
-        secondPointer++;
-    }
-
-
-    return xmlTree;
-
-}
-string XMLToJson::addQuotes(string element){
-    return "\"" + element + "\"";
-}
-vector<string> XMLToJson::ToVector(string elements){
-    vector<string> output;
-    string element;
-    int firstPointer=0;
-    int secondPointer=0;
-    while(secondPointer < elements.length()-2){
-        secondPointer = elements.find("<>", firstPointer);
-        element = elements.substr(firstPointer,secondPointer-firstPointer);
-        if(element.find(' ') && element[0] != ';'){
-            int i = 0;
-            while(element[i] == ' ')
-                i++;
-            element = element.substr(i,element.length() - i);
-            element = element.substr(0,element.find(' '));
-        }
-        output.push_back(element);
-        firstPointer = secondPointer + 2;
-    }   
-    return output;
-}
-
 
 XMLToJson::~XMLToJson()
 {
