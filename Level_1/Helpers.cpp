b075e1c401b52f56e03118bdb9d44dd72593ba5d
Abdallah Gabr <132190312+AbdallahGabr11@users.noreply.github.com>
2024-01-08 02:43:22 +0200

    Update Helpers.cpp

--- Level_1/Helpers.cpp
+++ Level_1/Helpers.cpp
@@ -1,35 +1,6 @@
 #include "Helpers.h"
 
 
-
-string Helpers::removeUnwantedSpaces(string XMLText) {
-    string output = "";
-    int startOfXML = XMLText.find('<');
-    output += XMLText[startOfXML];
-    char previousCharacter = output[0];
-
-    for (int i = startOfXML + 1; i < XMLText.length(); i++) {
-
-        if (XMLText[i] == ' ' || XMLText[i] == '\n') {
-            if (previousCharacter == ' ' || previousCharacter == '<' || previousCharacter == '>' || previousCharacter == '/' || previousCharacter == '?') {
-                continue;
-            }
-            output += ' ';
-        }else if(XMLText[i] == '<' || XMLText[i] == '>' || (XMLText[i] == '?' && i<XMLText.length()-1 && XMLText[i+1]== '>') || (XMLText[i] == '/' && i<XMLText.length()-1 && XMLText[i+1]== '>')) {
-            if (previousCharacter == ' ') {
-                output[output.length() - 1] = XMLText[i];
-            } else {
-                output += XMLText[i];
-            }
-
-        } else {
-            output += XMLText[i];
-        }
-        previousCharacter = output[output.length() - 1];
-    }
-
-    return output;
-}
 string Helpers::convertXML(const string& input) {
 		int attributePos = input.find(" ");
 		if (attributePos == -1) {
